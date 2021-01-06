aa8244dc165a48a881e646e7d4773058a8d6b659
변지현 <42201356+bjih1999@users.noreply.github.com>
2021-01-07 00:20:46 +0900

    fix/mail

--- WhatNext/settings.py
+++ WhatNext/settings.py
@@ -137,8 +137,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'bjih1999@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
 
-EMAIL_HOST_PASSWORD = '752727pg'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_USE_TLS = True
